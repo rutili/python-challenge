b5d707af5011e892da968a68ff78b22b0c711a2c
rutili <85592867+rutili@users.noreply.github.com>
2021-07-20 17:46:28 -0500

    election poll result tabulation, budgetary analysis, final version

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,22 +1,23 @@
 
 import os
-
 import csv
 
+#Input Source
 csvpath = os.path.join(r'C:\Users\joeru\Documents\GitHub\python-challenge\PyBank\Resources\budget_data.csv')
 
+# needed Lists
 Month = []
 Amount = []
 MonthChange = []
+OutPutLine = []
 
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
+    #print(csvreader)
 
     csv_header = next(csvreader)
-   # print(f"CSV Header: {csv_header}")
     
     MonthPLChange = 0
     LastMonthAmount = int(0)
@@ -31,32 +32,42 @@ with open(csvpath) as csvfile:
         Amount.append(row[1])
         if LastMonthAmount == 0: LastMonthAmount=int(row[1])
         
+        #Calc Monthly P/L Change
         MonthPLChange = int(row[1])-(LastMonthAmount)
 
+        #add Monthly Change
         MonthChange.append(int(MonthPLChange))
 
+#Calculations / Agrregations
 Mths=(len(Month))
 MXMC=(max(MonthChange)) 
 MNMC=(min(MonthChange))
 SMC=(sum(MonthChange))
-AVECH=(sum(MonthChange)/len(MonthChange))
+AVECH=((sum(MonthChange))/len(MonthChange))
 
+MXMCI=MonthChange.index(MXMC)
+MNMCI=MonthChange.index(MNMC)
 
-print("Financial Analysis") 
-print("_______________________________________________")
-print("Total Months:" + str(Mths))
-print("Average Change: " + str(AVECH))
-print("Greatest Increase in Profits" + str(MXMC))
-print("Greatest Decrease in Profits" + str(MNMC))
+MXCHM=Month[MXMCI]
+MNCHM=Month[MNMCI]
+
+#Build Output List
+OutPutLine.append(str("Financial Analysis"))
+OutPutLine.append(str("_______________________________________________"))
+OutPutLine.append(str("Total Months:" + str(Mths)))
+OutPutLine.append(str("Total:" + str(SMC)))
+OutPutLine.append(str("Average Change: " + str(round(AVECH,2))))
+OutPutLine.append(str("Greatest Increase in Profits " + str(MXCHM)+ " "+ str(MXMC)))
+OutPutLine.append(str("Greatest Decrease in Profits " + str(MNCHM)+ " "+ str(MNMC)))
+OPLLen = len(OutPutLine)
+
+#Dump output put
 
 output_path = os.path.join(r'C:\Users\joeru\Documents\GitHub\python-challenge\PyBank\Resources\Analysis.csv')
+with open(output_path, 'w', newline='\n') as csvfile:
+    writer = csv.writer(csvfile, delimiter ='\n')
+    writer.writerow(((OutPutLine)))
 
-with open(output_path, 'w', newline='') as csvfile:
-    writer=csv.writer(csvfile)
-
-    writer.writerow("Financial Analysis")
-    writer.writerow("_______________________________________________")
-    writer.writerow("Total Months:  " + str(Mths))
-    writer.writerow("Average Change:  " + str(AVECH))
-    writer.writerow("Greatest Increase in Profits  " + str(MXMC))
-    writer.writerow("Greatest Decrease in Profits  " + str(MNMC))
+for x in range(0, OPLLen):
+    print(str(OutPutLine[x]))
+    
